881fa09f955f23106080b13209afefd90b161697
Innei <tukon479@gmail.com>
2025-06-13 00:22:32 +0800

    refactor: migrate from react-query to react-router and clean up related code
    - Replaced '@tanstack/react-query' with '@tanstack/react-router' in package.json.
    - Removed the defineQuery and query-client modules, simplifying the state management approach.
    - Updated the App component to prefetch the preview page route.
    - Adjusted the RootProviders to remove the QueryClientProvider, streamlining the provider setup.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,15 @@
 import { inject } from '@vercel/analytics'
+import { useEffect } from 'react'
 import { Outlet } from 'react-router'
 
 import { RootProviders } from './providers/root-providers'
 
 inject()
-
+// prefetch preview page route
 function App() {
+  useEffect(() => {
+    import('~/pages/(main)/[photoId]/index')
+  }, [])
   return (
     <RootProviders>
       <div className="overflow-hidden lg:h-svh">
--- apps/web/src/providers/root-providers.tsx
+++ apps/web/src/providers/root-providers.tsx
@@ -1,11 +1,10 @@
-import { QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'jotai'
 import { LazyMotion, MotionConfig } from 'motion/react'
 import type { FC, PropsWithChildren } from 'react'
 
 import { Toaster } from '~/components/ui/sonner'
 import { jotaiStore } from '~/lib/jotai'
-import { queryClient } from '~/lib/query-client'
+import { Spring } from '~/lib/spring'
 
 import { ContextMenuProvider } from './context-menu-provider'
 import { EventProvider } from './event-provider'
@@ -17,22 +16,14 @@ const loadFeatures = () =>
   import('../framer-lazy-feature').then((res) => res.default)
 export const RootProviders: FC<PropsWithChildren> = ({ children }) => (
   <LazyMotion features={loadFeatures} strict key="framer">
-    <MotionConfig
-      transition={{
-        type: 'tween',
-        duration: 0.15,
-        ease: 'easeInOut',
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <Provider store={jotaiStore}>
-          <EventProvider />
-          <StableRouterProvider />
-          <SettingSync />
-          <ContextMenuProvider />
-          <I18nProvider>{children}</I18nProvider>
-        </Provider>
-      </QueryClientProvider>
+    <MotionConfig transition={Spring.presets.smooth}>
+      <Provider store={jotaiStore}>
+        <EventProvider />
+        <StableRouterProvider />
+        <SettingSync />
+        <ContextMenuProvider />
+        <I18nProvider>{children}</I18nProvider>
+      </Provider>
     </MotionConfig>
     <Toaster />
   </LazyMotion>
