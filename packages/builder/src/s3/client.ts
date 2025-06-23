5910ac3320d0e14b2d16ef305fb2c629c06f17fe
Innei <tukon479@gmail.com>
2025-06-23 11:39:32 +0800

    refactor: enhance S3 client configuration and error handling
    - Updated the S3 client creation logic to utilize builderConfig for improved configuration management.
    - Added error handling to ensure that the storage provider is set to 's3' and that required credentials are provided.
    - Streamlined the S3 client configuration by directly using values from builderConfig, enhancing maintainability.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- packages/builder/src/s3/client.ts
+++ packages/builder/src/s3/client.ts
@@ -1,28 +1,31 @@
 import type { S3ClientConfig } from '@aws-sdk/client-s3'
 import { S3Client } from '@aws-sdk/client-s3'
-import { env } from '@env'
+import { builderConfig } from '@builder'
 
 // 创建 S3 客户端
 function createS3Client(): S3Client {
-  if (!env.S3_ACCESS_KEY_ID || !env.S3_SECRET_ACCESS_KEY) {
-    throw new Error('S3_ACCESS_KEY_ID and S3_SECRET_ACCESS_KEY are required')
+  const storageConfig = builderConfig.storage
+
+  if (storageConfig.provider !== 's3') {
+    throw new Error('Storage provider is not s3')
+  }
+
+  const { accessKeyId, secretAccessKey, endpoint, region } = storageConfig
+  if (!accessKeyId || !secretAccessKey) {
+    throw new Error('accessKeyId and secretAccessKey are required')
   }
 
   const s3ClientConfig: S3ClientConfig = {
-    region: env.S3_REGION,
+    region,
     credentials: {
-      accessKeyId: env.S3_ACCESS_KEY_ID,
-      secretAccessKey: env.S3_SECRET_ACCESS_KEY,
+      accessKeyId,
+      secretAccessKey,
     },
     // from https://github.com/aws/aws-sdk-js-v3/issues/6810
     // some non AWS services like backblaze or cloudflare don't expect the new headers
     requestChecksumCalculation: 'WHEN_REQUIRED',
     responseChecksumValidation: 'WHEN_REQUIRED',
-  }
-
-  // 如果提供了自定义端点，则使用它
-  if (env.S3_ENDPOINT) {
-    s3ClientConfig.endpoint = env.S3_ENDPOINT
+    endpoint,
   }
 
   return new S3Client(s3ClientConfig)
