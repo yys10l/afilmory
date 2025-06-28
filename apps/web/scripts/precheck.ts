1337069c74007eb27db6215eaa7102aac2cf9101
Innei <tukon479@gmail.com>
2025-06-28 14:39:00 +0800

    refactor: simplify precheck script and remove pull-remote functionality
    - Streamlined the precheck script by removing checks for existing directories and the builder configuration.
    - Eliminated the pull-remote script, consolidating its functionality directly into the precheck process.
    - The updated precheck now directly runs the builder command, enhancing efficiency and reducing complexity.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/web/scripts/precheck.ts
+++ apps/web/scripts/precheck.ts
@@ -1,38 +1,14 @@
-import { existsSync } from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-import { builderConfig } from '@builder'
 import { $ } from 'execa'
-
-import { pullAndLinkRemoteRepo } from './pull-remote'
 
 export const precheck = async () => {
   const __dirname = path.dirname(fileURLToPath(import.meta.url))
   const workdir = path.resolve(__dirname, '..')
 
-  // 检查是否存在 public/thumbnails 和 src/data/photos-manifest.json
-  const thumbnailsDir = path.resolve(workdir, 'public', 'thumbnails')
-  const photosManifestPath = path.resolve(
-    workdir,
-    'src',
-    'data',
-    'photos-manifest.json',
-  )
-  const isExistThumbnails = existsSync(thumbnailsDir)
-  const isExistPhotosManifest = existsSync(photosManifestPath)
-
-  const shouldDoBuildOrClone = !isExistThumbnails || !isExistPhotosManifest
-
-  // 检查 builder 配置
-  if (shouldDoBuildOrClone) {
-    if (builderConfig.repo.enable) {
-      await pullAndLinkRemoteRepo()
-    } else {
-      await $({
-        cwd: workdir,
-        stdio: 'inherit',
-      })`pnpm --filter @afilmory/builder cli`
-    }
-  }
+  await $({
+    cwd: workdir,
+    stdio: 'inherit',
+  })`pnpm --filter @afilmory/builder cli`
 }
