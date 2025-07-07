1e834bf7db9ea397c0c1cf05b1995be03ab3f7ac
Innei <tukon479@gmail.com>
2025-07-07 23:35:53 +0800

    feat: enhance SSR route handling with DOM manipulation
    - Integrated DOMParser from linkedom to parse the HTML document.
    - Injected configuration into the document using the new injectConfigToDocument function.
    - Updated the response to return the modified HTML for improved server-side rendering.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/ssr/src/app/route.ts
+++ apps/ssr/src/app/route.ts
@@ -1,13 +1,18 @@
+import { DOMParser } from 'linkedom'
 import type { NextRequest } from 'next/server'
 
-import indexHtml from '../index.html'
+import { injectConfigToDocument } from '~/lib/injectable'
 
 export const GET = async (req: NextRequest) => {
   if (process.env.NODE_ENV === 'development') {
     return import('./[...all]/dev').then((m) => m.handler(req))
   }
 
-  return new Response(indexHtml, {
+  const indexHtml = await import('../index.html').then((m) => m.default)
+
+  const document = new DOMParser().parseFromString(indexHtml, 'text/html')
+  injectConfigToDocument(document)
+  return new Response(document.documentElement.outerHTML, {
     headers: {
       'Content-Type': 'text/html',
       'X-SSR': '1',
